c4381aa40bbf259f67dc90596db4bc050f335167
CaioLk PROGRAMADOR <99497978+caiodds@users.noreply.github.com>
2022-10-09 10:52:14 -0300

    Add files via upload

---
+++ dados/src/public/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"
+    integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK"
+    crossorigin="anonymous"></script>
+    <title>JS Project</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-dark text-light">
+  <div class="container-fluid">
+    <img src="\img\1156725_finance_logo_payment_ideal_icon.png" alt=""><p>Seu site de noticias</p>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item ">
+        <a class="nav-link active text-light " aria-current="page" href="" data-target="inicio.html"><i class="fa fa-home"></i> Inicio</a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link text-light " href="" data-target="contato.html"><i class="fas fa-address-book"></i> Contato</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="" data-target="sobre.html"><i class="fas fa-cog"></i>Sobre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled text-light" href="" tabindex="-1" aria-disabled="true"><i class="fas fa-bell-slash"></i>Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div id="app"></div>
+    <script src="/js/app.js"></script>
+</body>
+</html>
